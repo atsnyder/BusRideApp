a447f88547e63f8f6d85752721965b142e219115
atsnyder <adam-snyder@uiowa.edu>
2014-04-22 16:01:43 -0500

    homepage

--- BusRide/src/com/example/busride/MainActivity.java
+++ BusRide/src/com/example/busride/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.busride;
 
+import java.util.Calendar;
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
@@ -58,6 +64,31 @@ public class MainActivity extends ActionBarActivity {
 			View rootView = inflater.inflate(R.layout.fragment_main, container,
 					false);
 			return rootView;
+		}
+	}
+	
+	public void showDatePickerDialog(View v) {
+	    DialogFragment newFragment = new DatePickerFragment();
+	    //newFragment.show(getSupportFragmentManager(), "datePicker");
+	}
+	
+	public static class DatePickerFragment extends DialogFragment 
+								implements DatePickerDialog.OnDateSetListener {
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			// Use the current date as the default date in the picker
+			final Calendar c = Calendar.getInstance();
+			int year = c.get(Calendar.YEAR);
+			int month = c.get(Calendar.MONTH);
+			int day = c.get(Calendar.DAY_OF_MONTH);
+
+			// Create a new instance of DatePickerDialog and return it
+			return new DatePickerDialog(getActivity(), this, year, month, day);
+		}
+
+		public void onDateSet(DatePicker view, int year, int month, int day) {
+			// Do something with the date chosen by the user
 		}
 	}
 
